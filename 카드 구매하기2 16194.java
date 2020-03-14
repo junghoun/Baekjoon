4644833a671b6300a94da8a9c390308a7a588459
junghoun <49318478+junghoun@users.noreply.github.com>
2020-03-14 20:48:18 +0900

    Add files via upload

---
+++ 카드 구매하기2 16194.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[N+1];
+		int[] money = new int[N+1];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i =1; i <= N; i++) {
+			money[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.fill(dp, Integer.MAX_VALUE);
+		
+		dp[0] = 0;
+		dp[1] = money[1];
+
+		for(int i =1; i<= N; i++) {
+			for(int j =1; j <=i; j++) {
+				dp[i] = Math.min(dp[i],  dp[i-j] + money[j]);
+			}
+		}		
+		
+		System.out.println(dp[N]);
+	}
+}
